55cd66a2c18e6f77d4df11bcfacd954043270fe0
allnightcat <allnightcat12@gmail.com>
2023-02-26 16:50:51 +0900

    로그인 기능 추가

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,9 @@ function App() {
       <Router>
         <div>
           <Routes>
-            <Route exact path="/" element={<LandingPage />} />
-            <Route exact path="/login" element={<LoginPage />} />
-            <Route exact path="/register" element={<RegisterPage />} />
+            <Route exact={true} path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
           </Routes>
         </div>
       </Router>
