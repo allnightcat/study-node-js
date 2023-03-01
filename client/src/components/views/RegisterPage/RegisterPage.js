a8078f9ff573f84c13a51c58c0a2d782ad36c1c1
allnightcat <allnightcat12@gmail.com>
2023-03-01 14:58:57 +0900

    회원가입 기능 추가

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,7 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../../../_actions/user_action";
+import { useNavigate } from "react-router-dom";
 
 function RegisterPage() {
-  return <div>RegisterPage</div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [Email, setEmail] = useState("");
+  const [Name, setName] = useState("");
+  const [Password, setPassword] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value);
+  };
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+  const onConfirmPasswordHandler = (event) => {
+    setConfirmPassword(event.currentTarget.value);
+  };
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (Password !== ConfirmPassword) {
+      return alert("비밀번호와 비밀번호 확인은 같아야 합니다.");
+    }
+
+    let body = {
+      email: Email,
+      name: Name,
+      password: Password,
+    };
+
+    dispatch(registerUser(body)).then((response) => {
+      if (response.payload.success) {
+        alert("회원가입이 성공적으로 완료되었습니다.");
+        return navigate("/login");
+      } else {
+        alert("회원가입에 실패하였습니다.");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>이메일</label>
+        <input type="email" value={Email} onChange={onEmailHandler}></input>
+        <label>이름</label>
+        <input type="text" value={Name} onChange={onNameHandler}></input>
+        <label>비밀번호</label>
+        <input
+          type="password"
+          value={Password}
+          onChange={onPasswordHandler}
+        ></input>
+        <label>비밀번호 확인</label>
+        <input
+          type="password"
+          value={ConfirmPassword}
+          onChange={onConfirmPasswordHandler}
+        ></input>
+        <br />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default RegisterPage;
